75e87b6d068ac0d3f33fbbfe115550f8a1aaa557
Oliver K Cohen <olly@Olivers-MacBook-Pro.local>
2022-10-02 18:42:51 -0500

    updated yarn.lock

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Nav from "./nav"
 
 const Layout = ({ children, categories, needsHomeButton, seo }) => (
   <>
-    <Nav categories={categories} needsHomeButton={needsHomeButton}  />\
+    <Nav categories={categories} needsHomeButton={needsHomeButton}  />
     {children}
   </>
 )
